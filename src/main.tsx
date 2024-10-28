f13587e8fa9f3e84b4f6bcbf3bc791d613de6805
beringui1611 <fortiniteberingui12@gmail.com>
2024-10-28 20:41:30 -0300

    test

--- src/main.tsx
+++ src/main.tsx
@@ -14,6 +14,7 @@ import "./input.css";
   const queryClient = new QueryClient();
 
   const projectId = `${import.meta.env.VITE_API_KEY}`;
+  console.log(projectId);
 
   const metadata = {
     name: 'AppKit',
